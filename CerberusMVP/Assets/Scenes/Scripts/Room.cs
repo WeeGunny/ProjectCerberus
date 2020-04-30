8bb06ab68b8bb025894e21ca27d9c8978964700c
Luke Simpson <lukewizzle@gmail.com>
2020-04-30 02:47:08 -0400

    Enemy Spawn and remove 3rd person
    Removed 3rd person assets both from asset store and one created by me. Added in basic enemy spawners with placeholders. Also added more desert themed skybox.

--- CerberusMVP/Assets/Scenes/Scripts/Room.cs
+++ CerberusMVP/Assets/Scenes/Scripts/Room.cs
@@ -1,13 +1,37 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Room : MonoBehaviour {
     // Array of available doorways
     public Doorway[] doorways;
     public MeshCollider meshCollider;
-
+    public List<GameObject> spawnPoints, usedSpawnPoints, enemyPrefabs;
+    public int numOfEnemies;
     // Get the bounding box of a room
     public Bounds RoomBounds {
         get { return meshCollider.bounds; }
+    }
+
+    private void Start() {
+        SpawnEnemies();
+    }
+
+    public void SpawnEnemies() {
+
+        for (int i = 0; i < numOfEnemies; i++) {
+            if (spawnPoints.Count == 0) {
+                //if there are no more empty spawn points break
+                break;
+            }
+            int randomEnemy = Random.Range(0, enemyPrefabs.Count);
+            int randomSpawnPoint = Random.Range(0, spawnPoints.Count);
+            Instantiate(enemyPrefabs[randomEnemy], spawnPoints[randomSpawnPoint].transform);
+            //adds spawn point to used spawn points, and removes it as an available spawn point.
+            usedSpawnPoints.Add(spawnPoints[randomSpawnPoint]);
+            spawnPoints.RemoveAt(randomSpawnPoint);
+        }
+
     }
 
 }
