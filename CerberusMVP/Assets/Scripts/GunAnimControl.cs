f92fbcc433f497c422684f2d21568041ddb0bccc
Rug <noah.price.237@gmail.com>
2020-05-16 17:41:04 -0400

    BlackHoleGun Animation controller, fixed material
    - Made the material for BlackHoleGun less ostentatious
    - Added GunFactory scene for testing gun controllers
    - Added GunAnimControl as a basic animation controller class
    - Added statemachine for BlackHoleGun
        - Some errors exist with my animations, not in the state machine. Need to
           realign the first frame of each animation to be identical to idle.

---
+++ CerberusMVP/Assets/Scripts/GunAnimControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunAnimControl : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        animator = GetComponent<Animator> ();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1"))
+        {
+            animator.SetBool("isShooting", true);
+        }
+        else {
+            animator.SetBool("isShooting", false);
+        }
+        
+        if (Input.GetButton("Reload")) {
+            animator.SetTrigger("isReloading");
+        }
+    }
+}
