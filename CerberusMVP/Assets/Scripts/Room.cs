2733601c2863049f76f508c8262f111399686aa2
Luke Simpson <lukewizzle@gmail.com>
2020-05-27 02:47:17 -0400

    added functions to room script

--- CerberusMVP/Assets/Scripts/Room.cs
+++ CerberusMVP/Assets/Scripts/Room.cs
@@ -17,8 +17,9 @@ public class Room : MonoBehaviour {
     public List<GameObject> itemPrefabs = new List<GameObject>();
     public List<GameObject> itemSPs = new List<GameObject>();
     private List<GameObject> usedItemSPs = new List<GameObject>();
-    public int numOfEnemies, numOfItems;
-    public int id;
+    public int enemiesToSpawn, itemsToSpawn;
+    public int enemiesAlive =0;
+    public int id = -1;
     // Get the bounding box of a room
     public Bounds RoomBounds {
         //get {return boxCollider.bounds}
@@ -27,12 +28,26 @@ public class Room : MonoBehaviour {
 
     private void Start() {
         //SpawnEnemies();
+        GameEvents.current.onDoorwayTriggerExit += LockDoors;
+        GameEvents.current.onEnemiesDefeated += UnlockDoors;
         SpawnItems();
+    }
+
+    private void Update()
+    {
+        if (id != -1)
+        {
+
+        }
+        if(enemiesAlive == 0)
+        {
+
+        }
     }
 
     public void SpawnEnemies() {
 
-        for (int i = 0; i < numOfEnemies; i++) {
+        for (int i = 0; i < enemiesToSpawn; i++) {
             if (enemySPs.Count == 0) {
                 //GameEvents.current.EnemiesDefeated(id);
                 //if there are no more empty spawn points break
@@ -45,13 +60,21 @@ public class Room : MonoBehaviour {
             //adds spawn point to used spawn points, and removes it as an available spawn point.
             usedSPs.Add(enemySPs[randomSpawnPoint]);
             enemySPs.RemoveAt(randomSpawnPoint);
+            enemiesAlive++;
         }
 
+    }
+    public void ResetEnemies()
+    {
+        foreach(GameObject spawnPoint in usedSPs)
+        {
+            enemySPs.Add(spawnPoint);
+        }
     }
 
     public void SpawnItems() {
 
-        for (int i = 0; i < numOfItems; i++) {
+        for (int i = 0; i < itemsToSpawn; i++) {
             if (itemSPs.Count == 0) {
                 //if there are no more empty spawn points break
                 break;
@@ -63,6 +86,41 @@ public class Room : MonoBehaviour {
             //adds spawn point to used spawn points, and removes it as an available spawn point.
             usedItemSPs.Add(itemSPs[randomSpawnPoint]);
             itemSPs.RemoveAt(randomSpawnPoint);
+        }
+    }
+
+    public void ResetItems()
+    {
+        foreach (GameObject spawnPoint in usedItemSPs)
+        {
+            itemSPs.Add(spawnPoint);
+        }
+
+    }
+
+
+
+    public void LockDoors(int id)
+    {
+        if(id == this.id)
+        {
+            foreach(Doorway doorway in doorways)
+            {
+                doorway.gameObject.SetActive(true);
+            }
+        }
+
+    }
+
+    public void UnlockDoors(int id)
+    {
+        if (id == this.id)
+        {
+            foreach (Doorway doorway in doorways)
+            {
+                doorway.gameObject.SetActive(false);
+            }
+
         }
     }
 }
