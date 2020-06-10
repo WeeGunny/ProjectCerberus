56ddd02f73d1d0d49902547c706abf5e414dac47
GoldenBayse <63566646+GoldenBayse@users.noreply.github.com>
2020-06-10 18:41:36 -0400

    Started Hub Area and Dialogue

---
+++ CerberusMVP/Assets/Scripts/NPC/DialogueManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Queue<string> sentences;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+    }
+}
---
+++ CerberusMVP/Assets/Scripts/NPC/DialogueTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialogue(Collider other)
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+
+        if(other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            //StartDialogue();
+        }
+    }
+
+}
