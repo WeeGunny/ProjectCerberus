0471694c383842abcc45baa7a8d25cf5ddbaad7f
GoldenBayse <63566646+GoldenBayse@users.noreply.github.com>
2020-08-11 18:03:33 -0400

    Boss 3

--- CerberusMVP/Assets/Scripts/Boss/Boss_Run.cs
+++ CerberusMVP/Assets/Scripts/Boss/Boss_Run.cs
@@ -13,14 +13,14 @@ public class Boss_Run : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<rbPlayer>().transform;
-        animator.GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody>();
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Vector3 target = new Vector3(player.position.x, rb.position.y);
+        Vector3 target = new Vector3(player.position.x, rb.position.y, rb.position.z);
         Vector3 newPos = Vector3.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
         rb.MovePosition(newPos);
     }
