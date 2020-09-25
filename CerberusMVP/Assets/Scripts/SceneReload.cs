25d31462ded058b0a92a51334abe4fac57f06172
GoldenBayse <63566646+GoldenBayse@users.noreply.github.com>
2020-09-25 14:11:59 -0400

    loading stuff 2

---
+++ CerberusMVP/Assets/Scripts/SceneReload.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneReload : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+
+            DontDestroyOnLoad(player);
+        }
+    }
+}
