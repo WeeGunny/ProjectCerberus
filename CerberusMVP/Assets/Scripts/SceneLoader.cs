90a4cd88dd4d1ac589ea4164e8904e6ad6415c21
GoldenBayse <63566646+GoldenBayse@users.noreply.github.com>
2020-09-25 13:25:43 -0400

    Loading Stuff

---
+++ CerberusMVP/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+
+    public void LoadScene(int sceneIndex)
+    {
+        StartCoroutine(LoadAsync(sceneIndex));
+    }
+
+    IEnumerator LoadAsync (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            //Fills slider based on loading progress
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+
+    }
+}
