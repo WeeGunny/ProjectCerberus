bfd561d7caa4ba248650b046706e3abd97362af9
ponyo27 <63566682+ponyo27@users.noreply.github.com>
2020-12-17 18:17:35 -0500

    Volume Menu
    Volume menu works (for the main menu, unsure how it will work with the audio manager Aran put in)

---
+++ CerberusMVP/Assets/OptionsMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolumeMaster(float MasterVolume)
+    {
+        audioMixer.SetFloat("MasterVolume", MasterVolume);
+    }
+
+    public void SetVolumeMusic(float MusicVolume)
+    {
+        audioMixer.SetFloat("MusicVolume", MusicVolume);
+    }
+
+    public void SetVolumeSFX(float SFXVolume)
+    {
+        audioMixer.SetFloat("SFXVolume", SFXVolume);
+    }
+}
