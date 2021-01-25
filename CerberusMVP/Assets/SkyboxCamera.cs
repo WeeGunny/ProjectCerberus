b06487bdeebb0da790865df9b33ce2aecd5a2e6e
Rug <noah.price.237@gmail.com>
2021-01-25 03:37:42 -0500

    Add 3D Skybox Groundwork
    Add 3D Skybox components.
    New 'overlay' camera
    New terrain
    'overlay' camera follows PlayerCamera transforms via SkyboxCamera script
    
    The culling works as intended, but the transforms absolutely do not.

---
+++ CerberusMVP/Assets/SkyboxCamera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxCamera : MonoBehaviour
+{
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private float skyboxScale;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = playerCamera.rotation;
+        //transform.localPosition = playerCamera.position / skyboxScale;    
+    }
+}
