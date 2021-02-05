ee9b751d72dd68eed2d1bc9617007e6a42c9a895
Rug <noah.price.237@gmail.com>
2021-02-05 00:17:23 -0500

    Add scene framework, SceneToggle script

---
+++ CerberusMVP/Assets/Scripts/Scene Toggle/SceneToggle.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneToggle : MonoBehaviour
+{
+    public Camera cam;
+    public RenderTexture rt;
+
+    
+    void toggleCamera() {
+        cam.
+
+    }
+}
