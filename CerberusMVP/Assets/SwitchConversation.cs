0e1bcb51152cccb68317b6d156c89fb3a6a43f20
Panayiotis Paleshi <63566682+ponyo27@users.noreply.github.com>
2021-05-03 19:06:15 -0400

    Jimbo now talks to you from the ground
    - When finishing the jumping tutorial section, Jimbo now encourages the player
    - My VisualStudio is now properly set up

---
+++ CerberusMVP/Assets/SwitchConversation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchConversation : MonoBehaviour
+{
+    public NPC Jimbo;
+    public GameObject gameUI;
+
+    public void OnCollisionEnter(Collision col)
+    {
+        GameObject hit = col.gameObject;
+        if (hit.tag == "Player")
+        {
+            Debug.Log("Collision detected!");
+            Conversation Jimbo2 = Jimbo.secondConversation;
+            DialogueManager.dm.StartDialog(Jimbo2, Jimbo);
+            gameObject.SetActive(false);
+            gameUI.SetActive(false);
+            NPC.playerIsTalking = true;
+            rbCam.LockCam();
+            DialogueManager.dm.nextButton.SetActive(true);
+        }
+    }
+}
