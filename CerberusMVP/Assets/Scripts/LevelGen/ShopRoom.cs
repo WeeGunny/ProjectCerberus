97d62f9b2330565b2a2174f2bc55f2a5297d92de
Luke Simpson <lukewizzle@gmail.com>
2021-05-15 16:29:39 -0400

    After merge cleanup
    Boss room still busted

--- CerberusMVP/Assets/Scripts/LevelGen/ShopRoom.cs
+++ CerberusMVP/Assets/Scripts/LevelGen/ShopRoom.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class ShopRoom : Room {
     public GameObject Display1, Display2, Display3;
+    bool displaysFilled =false;
     // Start is called before the first frame update
     void Start() {
-        ShopUI.shopUI.FillDisplays(Display1, Display2, Display3);
+        if (ShopUI.shopUI) {
+            ShopUI.shopUI.FillDisplays(Display1, Display2, Display3);
+            displaysFilled = true;
+        }
+       
     }
 
     // Update is called once per frame
     void Update() {
 
+        if(displaysFilled == false && ShopUI.shopUI) {
+            ShopUI.shopUI.FillDisplays(Display1, Display2, Display3);
+            displaysFilled = true;
+        }
     }
 }
